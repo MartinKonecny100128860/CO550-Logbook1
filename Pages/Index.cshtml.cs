90e8bf25fe7cd74468bdc32f8f67d87a1cfb1d4a
MartinKonecny100128860 <100128860@student.burnley.ac.uk>
2023-10-06 22:35:42 +0100

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Primitives;
+
+namespace CoreTestApp.Pages
+{
+    public class NameMessageTaskModel : PageModel
+    {
+        public StringValues name;
+        public void OnGet()
+        {
+
+        }
+        public void OnPost() 
+        { 
+            this.name = Request.Form["name"];
+        }
+    }
+}
+
